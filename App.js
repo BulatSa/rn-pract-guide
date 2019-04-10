352730ef518b3cdda595b1bcbe8a176c82fe102a
Bulat <sadikovba@gmail.com>
2019-04-10 18:08:09 +0300

    8. Add tab navigation

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import { Navigation } from 'react-native-navigation';
 
 import AuthScreen from './src/screens/auth/auth';
+import SharePlaceScreen from './src/screens/share-place/share-place';
+import FindPlaceScreen from './src/screens/find-place/find-place';
 
 // Register Screens
 Navigation.registerComponent('awesome-places.AuthScreen', () => AuthScreen);
+Navigation.registerComponent('awesome-places.SharePlaceScreen', () => SharePlaceScreen);
+Navigation.registerComponent('awesome-places.FindPlaceScreen', () => FindPlaceScreen);
 
 // Start a App
 Navigation.startSingleScreenApp({
