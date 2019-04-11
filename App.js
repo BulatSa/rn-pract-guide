82b17cf520246176ed8614357789f5f8366274c2
Bulat <sadikovba@gmail.com>
2019-04-11 11:00:46 +0300

    Add a Side Drawer

--- App.js
+++ App.js
@@ -3,11 +3,14 @@ import { Navigation } from 'react-native-navigation';
 import AuthScreen from './src/screens/auth/auth';
 import SharePlaceScreen from './src/screens/share-place/share-place';
 import FindPlaceScreen from './src/screens/find-place/find-place';
+import SideDrawer from './src/screens/side-drawer/side-drawer';
 
 // Register Screens
 Navigation.registerComponent('awesome-places.AuthScreen', () => AuthScreen);
 Navigation.registerComponent('awesome-places.SharePlaceScreen', () => SharePlaceScreen);
 Navigation.registerComponent('awesome-places.FindPlaceScreen', () => FindPlaceScreen);
+
+Navigation.registerComponent('awesome-places.SideDrawer', () => SideDrawer);
 
 // Start a App
 Navigation.startSingleScreenApp({
